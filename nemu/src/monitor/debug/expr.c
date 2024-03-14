9b9a9dc73eca792ad177209edbf25dfeaefc70c2
tracer-ics2017 <tracer@njuics.org>
2024-03-14 03:16:51 -0700

    >  compile 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:16:51 up 3 days, 22:11,  4 users,  load average: 0.29, 0.41, 0.41 13acf174bccdf1d85a2828b11d967c3431827195

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -364,15 +364,15 @@ uint32_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   // 处理单目运算
-  for (int i = 0; i < nr_token; i++) {
-    if (tokens[i].type == '*' || tokens[i].type == '-') {
-      if (i == 0 ||
-          (tokens[i - 1].type != TK_DEC && tokens[i - 1].type != TK_HEX &&
-           tokens[i - 1].type != TK_REG && tokens[i - 1].type != ')')) {
-        tokens[i].type = tokens[i].type == '*' ? TK_DEREF : TK_NEG;
-      }
-    }
-  }
+  // for (int i = 0; i < nr_token; i++) {
+  //   if (tokens[i].type == '*' || tokens[i].type == '-') {
+  //     if (i == 0 ||
+  //         (tokens[i - 1].type != TK_DEC && tokens[i - 1].type != TK_HEX &&
+  //          tokens[i - 1].type != TK_REG && tokens[i - 1].type != ')')) {
+  //       tokens[i].type = tokens[i].type == '*' ? TK_DEREF : TK_NEG;
+  //     }
+  //   }
+  // }
   int value = eval(0, nr_token - 1, success);
   return value;
 }
