b457f547d5f26bfadd5e04134f136639f73d5ab7
tracer-ics2017 <tracer@njuics.org>
2024-04-01 18:53:57 -0700

    >  compile 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:53:57 up 1 day, 12:16,  1 user,  load average: 0.11, 0.06, 0.01 2ac0bf8c415abfb4d2e0d9fa84b502eee379793f

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,8 +1,8 @@
 #include "cpu/exec.h"
 
 make_EHelper(test) {
-  rtl_and(&t0,&id_dest->val,&id_src->val);
-  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_and(&t0, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t0, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
 
@@ -10,10 +10,10 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  rtl_and(&t2,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t2);
+  rtl_and(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
 
-  rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
 
@@ -21,10 +21,10 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  rtl_xor(&t2,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t2);
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
 
-  rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
 
@@ -32,10 +32,10 @@ make_EHelper(xor) {
 }
 
 make_EHelper(or) {
-  rtl_or(&t3,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t3);
+  rtl_or(&t3, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t3);
 
-  rtl_update_ZFSF(&t3,id_dest->width);
+  rtl_update_ZFSF(&t3, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
 
@@ -43,27 +43,27 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  rtl_sar(&t2,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t2);
-  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+  rtl_sar(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  rtl_shl(&t2,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t2);
-  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+  rtl_shl(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  rtl_shr(&t0,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t0);
-  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+  rtl_shr(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
@@ -78,18 +78,18 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  rtl_mv(&t1,&id_dest->val);
+  rtl_mv(&t1, &id_dest->val);
   rtl_not(&t1);
-  operand_write(id_dest,&t1);
+  operand_write(id_dest, &t1);
 
   print_asm_template1(not);
 }
 
 make_EHelper(rol) {
-	rtl_shri(&t2, &id_dest->val, id_dest->width * 8 - id_src->val);
-	rtl_shl(&t3, &id_dest->val, &id_src->val);
-	rtl_or(&t1, &t2, &t3);
-	operand_write(id_dest, &t1);
+  rtl_shri(&t2, &id_dest->val, id_dest->width * 8 - id_src->val);
+  rtl_shl(&t3, &id_dest->val, &id_src->val);
+  rtl_or(&t1, &t2, &t3);
+  operand_write(id_dest, &t1);
 
   print_asm_template2(rol);
 }
