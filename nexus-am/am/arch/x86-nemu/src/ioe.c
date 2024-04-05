b6630bb7dbb0827345ed6d5119ca9ea87219e624
tracer-ics2017 <tracer@njuics.org>
2024-04-04 22:40:28 -0700

    >  compile 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  22:40:28 up 1 day, 16:02,  1 user,  load average: 0.01, 0.19, 0.16 af58114236d67626b68296fc53f3543825f706ab

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,25 +1,24 @@
 #include <am.h>
 #include <x86.h>
 
-#define RTC_PORT 0x48   // Note that this is not standard
+#define RTC_PORT 0x48 // Note that this is not standard
 static unsigned long boot_time;
 
-void _ioe_init() {
-  boot_time = inl(RTC_PORT);
-}
+void _ioe_init() { boot_time = inl(RTC_PORT); }
 
 unsigned long _uptime() {
-  return 0;
+  unsigned long time = inl(RTC_PORT) - boot_time;
+  return time;
 }
 
-uint32_t* const fb = (uint32_t *)0x40000;
+uint32_t *const fb = (uint32_t *)0x40000;
 
 _Screen _screen = {
-  .width  = 400,
-  .height = 300,
+    .width = 400,
+    .height = 300,
 };
 
-extern void* memcpy(void *, const void *, int);
+extern void *memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
@@ -28,9 +27,12 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   }
 }
 
-void _draw_sync() {
-}
+void _draw_sync() {}
 
 int _read_key() {
+  printf("test");
+  if (inb(0x64)) {
+    return inl(0x60);
+  }
   return _KEY_NONE;
 }
