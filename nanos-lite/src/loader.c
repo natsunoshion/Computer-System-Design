fc08dc6b99eb78e80b5bb4618ad9fead36a43b41
tracer-ics2017 <tracer@njuics.org>
2024-04-26 06:34:31 -0700

    >  run 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:34:31 up  5:48,  2 users,  load average: 0.03, 0.05, 0.06 24e51be28ac3e123899ce70930410cf2db127e64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,7 +14,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
   // ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
   int fd = fs_open(filename, 0, 0);
-  Log("filename=%s,fd=%d",filename,fd);
+  Log("filename=%s,fd=%d", filename, fd);
   fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
