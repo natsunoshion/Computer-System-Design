96672c1b2f4a727ab4f7ac7598770c1532935aee
tracer-ics2017 <tracer@njuics.org>
2024-05-26 02:53:28 -0700

    >  run 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:53:28 up 17:35,  2 users,  load average: 0.30, 0.71, 1.38 bde4f08a6838d8d4a233710fa163d5fc631807d0

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -4,33 +4,32 @@
 
 static PDE kpdirs[NR_PDE] PG_ALIGN;
 static PTE kptabs[PMEM_SIZE / PGSIZE] PG_ALIGN;
-static void* (*palloc_f)();
-static void (*pfree_f)(void*);
+static void *(*palloc_f)();
+static void (*pfree_f)(void *);
 
-extern void *memcpy(void *,const void*,int);
+extern void *memcpy(void *, const void *, int);
 
-_Area segments[] = {      // Kernel memory mappings
-  {.start = (void*)0,          .end = (void*)PMEM_SIZE}
-};
+_Area segments[] = { // Kernel memory mappings
+    {.start = (void *)0, .end = (void *)PMEM_SIZE}};
 
 #define NR_KSEG_MAP (sizeof(segments) / sizeof(segments[0]))
 
-void _pte_init(void* (*palloc)(), void (*pfree)(void*)) {
+void _pte_init(void *(*palloc)(), void (*pfree)(void *)) {
   palloc_f = palloc;
   pfree_f = pfree;
 
   int i;
 
   // make all PDEs invalid
-  for (i = 0; i < NR_PDE; i ++) {
+  for (i = 0; i < NR_PDE; i++) {
     kpdirs[i] = 0;
   }
 
   PTE *ptab = kptabs;
-  for (i = 0; i < NR_KSEG_MAP; i ++) {
+  for (i = 0; i < NR_KSEG_MAP; i++) {
     uint32_t pdir_idx = (uintptr_t)segments[i].start / (PGSIZE * NR_PTE);
     uint32_t pdir_idx_end = (uintptr_t)segments[i].end / (PGSIZE * NR_PTE);
-    for (; pdir_idx < pdir_idx_end; pdir_idx ++) {
+    for (; pdir_idx < pdir_idx_end; pdir_idx++) {
       // fill PDE
       kpdirs[pdir_idx] = (uintptr_t)ptab | PTE_P;
 
@@ -39,7 +38,7 @@ void _pte_init(void* (*palloc)(), void (*pfree)(void*)) {
       PTE pte_end = PGADDR(pdir_idx + 1, 0, 0) | PTE_P;
       for (; pte < pte_end; pte += PGSIZE) {
         *ptab = pte;
-        ptab ++;
+        ptab++;
       }
     }
   }
@@ -49,58 +48,53 @@ void _pte_init(void* (*palloc)(), void (*pfree)(void*)) {
 }
 
 void _protect(_Protect *p) {
-  PDE *updir = (PDE*)(palloc_f());
+  PDE *updir = (PDE *)(palloc_f());
   p->ptr = updir;
   // map kernel space
-  for (int i = 0; i < NR_PDE; i ++) {
+  for (int i = 0; i < NR_PDE; i++) {
     updir[i] = kpdirs[i];
   }
 
-  p->area.start = (void*)0x8000000;
-  p->area.end = (void*)0xc0000000;
+  p->area.start = (void *)0x8000000;
+  p->area.end = (void *)0xc0000000;
 }
 
-void _release(_Protect *p) {
-}
+void _release(_Protect *p) {}
 
-void _switch(_Protect *p) {
-  set_cr3(p->ptr);
-}
+void _switch(_Protect *p) { set_cr3(p->ptr); }
 
 void _map(_Protect *p, void *va, void *pa) {
-  PDE *pgdir=(PDE*)p->ptr; 
-  PTE *pgtab=NULL; 
+  PDE *pgdir = (PDE *)p->ptr;
+  PTE *pgtab = NULL;
 
-  PDE *pde = pde=pgdir+PDX(va); 
-  if(!(*pde&PTE_P)){ 
-    pgtab=(PTE*)(palloc_f()); 
-    *pde=(uintptr_t)pgtab|PTE_P; 
-  } 
-  
-  pgtab=(PTE*)PTE_ADDR(*pde);
-  PTE *pte=pgtab+PTX(va);
-  *pte=(uintptr_t)pa|PTE_P; 
+  PDE *pde = pde = pgdir + PDX(va);
+  if (!(*pde & PTE_P)) {
+    pgtab = (PTE *)(palloc_f());
+    *pde = (uintptr_t)pgtab | PTE_P;
+  }
+
+  pgtab = (PTE *)PTE_ADDR(*pde);
+  PTE *pte = pgtab + PTX(va);
+  *pte = (uintptr_t)pa | PTE_P;
 }
 
-void _unmap(_Protect *p, void *va) {
-}
+void _unmap(_Protect *p, void *va) {}
 
+_RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry,
+                char *const argv[], char *const envp[]) {
 
-_RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
-  
-  int arg1=0;
-  char *arg2=NULL;
-  memcpy((void*)ustack.end-4,(void*)arg2,4);
-  memcpy((void*)ustack.end-8,(void*)arg2,4);
-  memcpy((void*)ustack.end-12,(void*)arg1,4);
-  memcpy((void*)ustack.end-16,(void*)arg1,4);
-  
-  
+  int arg1 = 0;
+  char *arg2 = NULL;
+  memcpy((void *)ustack.end - 4, (void *)arg2, 4);
+  memcpy((void *)ustack.end - 8, (void *)arg2, 4);
+  memcpy((void *)ustack.end - 12, (void *)arg1, 4);
+  memcpy((void *)ustack.end - 16, (void *)arg1, 4);
+
   _RegSet tf;
-  tf.cs=8;
-  tf.eflags=0x202;
-  tf.eip=(uintptr_t)entry; 
-  void *tf_addr=(void*)(ustack.end-16-sizeof(tf));
-  memcpy(tf_addr,(void*)&tf,sizeof(tf));
-  return (_RegSet*)tf_addr;
+  tf.cs = 8;
+  tf.eflags = 0x202;
+  tf.eip = (uintptr_t)entry;
+  void *tf_addr = (void *)(ustack.end - 16 - sizeof(tf));
+  memcpy(tf_addr, (void *)&tf, sizeof(tf));
+  return (_RegSet *)tf_addr;
 }
