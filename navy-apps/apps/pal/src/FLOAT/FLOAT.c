6ad32848c71986ad60b49b451b54fbe086a95252
tracer-ics2017 <tracer@njuics.org>
2024-06-06 23:00:21 -0700

    >  compile 2113958 lollikit Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  23:00:21 up 7 days, 13:25,  2 users,  load average: 1.75, 2.04, 1.47 865373fdbbdbca015141ee3a2d4a0479a3fe45e9

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -2,36 +2,36 @@
 #include <assert.h>
 #include <stdint.h>
 
-typedef union {
+union float_ {
   struct {
-    uint32_t m : 23;
-    uint32_t e : 8;
-    uint32_t s : 1;
+    uint32_t man : 23;
+    uint32_t exp : 8;
+    uint32_t sign : 1;
   };
   uint32_t val;
-} Float;
+};
 
-#define __sign(x) ((x)&0x80000000)
-#define __scale(x) (__sign(x) ? -(x) : (x))
-
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  int64_t scale = ((int64_t)a * (int64_t)b) >> 16;
-  return scale;
-}
+FLOAT F_mul_F(FLOAT a, FLOAT b) { return ((int64_t)a * (int64_t)b) >> 16; }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  int64_t x = __scale(a) * 65536ll;
-  int64_t y = __scale(b);
-  int64_t ans = 0;
-  for (int i = 31; i >= 0; i--) {
-    int64_t t = y * (1ll << i);
-    if (t <= x) {
-      x -= t;
-      ans += (1ll << i);
+  assert(b != 0);
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT ret = x / y;
+  x = x % y;
+
+  for (int i = 0; i < 16; i++) {
+    x <<= 1;
+    ret <<= 1;
+    if (x >= y) {
+      x -= y;
+      ret++;
     }
   }
-  ans *= ((__sign(a) ^ __sign(b)) ? -1 : 1);
-  return ans;
+  if (((a ^ b) & 0x80000000) == 0x80000000) {
+    ret = -ret;
+  }
+  return ret;
 }
 
 FLOAT f2F(float a) {
@@ -45,21 +45,29 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  Float f;
-  void *tmp = &a;
-  f.val = *(uint32_t *)tmp;
-  uint32_t m = f.m | (1 << 23);
-  int shift = 134 - (int)f.e;
-  if (shift < 0) {
-    m <<= (-shift);
-  } else {
-    m >>= shift;
-  }
-  return (__sign(f.val) ? -m : m);
-  ;
+  union float_ f;
+  f.val = *((uint32_t *)(void *)&a);
+  int exp = f.exp - 127;
+  FLOAT ret = 0;
+  if (exp == 128)
+    assert(0);
+  if (exp >= 0) {
+    int mov = 7 - exp;
+    if (mov >= 0)
+      ret = (f.man | (1 << 23)) >> mov;
+    else
+      ret = (f.man | (1 << 23)) << (-mov);
+  } else
+    return 0;
+  return f.sign == 0 ? ret : -ret;
 }
 
-FLOAT Fabs(FLOAT a) { return __scale(a); }
+FLOAT Fabs(FLOAT a) {
+  if ((a & 0x80000000) == 0)
+    return a;
+  else
+    return -a;
+}
 
 /* Functions below are already implemented */
 
